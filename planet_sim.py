d1c1eda2cb3ab41bbb6c9a6c92ce76233a75a25f
mackanh123 <markus.hjalt1@gmail.com>
2023-08-30 20:28:31 +0200

    Update planet_sim.py
    Added some stationary planets on a display

--- planet_sim.py
+++ planet_sim.py
@@ -7,8 +7,17 @@ WIN = pygame.display.set_mode((WIDTH, HEIGHT)) # creates the pygame window
 pygame.display.set_caption("Planet Simulation")
 
 WHITE = (255, 255, 255) # define color white
+YELLOW = (255, 255, 0)
+BLUE = (100, 149, 237)
+RED = (188, 39, 50)
+DARK_GREY = (80, 78, 81)
 
 class Planet:
+    AU = 149.6e6 * 1000 # 1AU in meter
+    G = 6.67428e-11
+    SCALE = 250 / AU # 1AU = 100 pixels
+    TIMESTEP = 3600*24 # 1 day
+
     def __init__(self, x, y, radius, color, mass):
         self.x = x
         self.y = y
@@ -16,12 +25,34 @@ class Planet:
         self.color = color
         self.mass = mass
 
+        self.orbit = []
+        self.sun = False
+        self.distance_to_sun = 0
+
         self.x_vel = 0
         self.y_vel = 0
+
+    def draw(self, win):
+        x = self.x * self.SCALE + WIDTH / 2
+        y = self.y * self.SCALE + HEIGHT / 2
+        pygame.draw.circle(win, self.color, (x,y), self.radius)
 
 def main():
     run = True
     clock = pygame.time.Clock() 
+
+    sun = Planet(0, 0, 30, YELLOW, 1.98892 * 10**30)
+    sun.sun = True
+
+    earth  = Planet(-1 * Planet.AU, 0, 16, BLUE, 5.9742 * 10**24)
+
+    mars = Planet(-1.524 * Planet.AU, 0, 12, RED, 6.39 * 10**23)
+
+    mercury = Planet(0.387 * Planet.AU, 0, 8, DARK_GREY, 0.330 * 10**24)
+
+    venus = Planet(0.723 * Planet.AU, 0, 14, WHITE, 4.8685 * 10**24)
+
+    planets = [sun, earth, mars, mercury, venus]
 
     while run: 
         clock.tick(60) # update screen max 60 times/sec
@@ -31,6 +62,12 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+
+        for planet in planets:
+            planet.draw(WIN)
+
+        pygame.display.update()
+    
     pygame.quit()
 
 main()
